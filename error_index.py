76529dea2b48264f2839b1c256f26b6143d3811a
Bakkolla Hemanth kumar <bakkollahemanthkumar@gmail.com>
2025-06-27 18:24:55 +0530

    finding value in string

--- error_index.py
+++ error_index.py
@@ -1,4 +1,4 @@
-#we are writing error program 
+#we are writing error program  
 name="hemanthkumar"
 print("given string is :",name)
 print("we are finding -2 indexing value in string is :",name[-9])
---
+++ practice_1.py
@@ -0,0 +1,7 @@
+name="harry potter"
+print("given string is :",name)
+print("we are finding -7 indexing value in string is :",name[-7])
+print("we are finding 8 indexing value in string is :",name[8])
+print("we are finding -9 indexing value in string is :",name[-9])
+print("we are finding -5 indexing value in string is :",name[-5])
+
