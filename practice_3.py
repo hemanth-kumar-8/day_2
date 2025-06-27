bc60789257ea8dd8f703df4dbbce3242785fe1e2
Bakkolla Hemanth kumar <bakkollahemanthkumar@gmail.com>
2025-06-27 18:38:54 +0530

    finding value in string

---
+++ practice_3.py
@@ -0,0 +1,7 @@
+name="sony bravia"
+print("given string is :",name)
+print("we are finding -6 indexing value in string is :",name[-6])
+print("we are finding 4 indexing value in string is :",name[4])
+print("we are finding 9 indexing value in string is :",name[9])
+print("we are finding 10 -11 indexing value in string is :",name[10],name[-11])
+
