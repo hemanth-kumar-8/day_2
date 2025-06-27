49a8ffe30d3c5f32ee2b2c8188dcf2b6cf55a1e4
Bakkolla Hemanth kumar <bakkollahemanthkumar@gmail.com>
2025-06-27 18:47:10 +0530

    finding value in string

---
+++ practice_4.py
@@ -0,0 +1,7 @@
+name="binladin"
+print("given string is :",name)
+print("we are finding -5 4 indexing value in string is :",name[-5],name[4])
+print("we are finding -7 indexing value in string is :",name[-7])
+print("we are finding -2 indexing value in string is :",name[-2])
+
+
